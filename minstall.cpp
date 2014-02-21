e72a51417a10959601cea88cd727d64d7b706b80
anticapitalista <antiX@operamail.com>
2014-02-21 14:58:03 +0200

    Changes to limit hostname

--- minstall.cpp
+++ minstall.cpp
@@ -66,7 +66,11 @@ MInstall::MInstall(QWidget *parent) : QWidget(parent) {
   kb = kb.section('=', 1);
   kb = kb.section(',', 0, 0);
   kb.remove(QChar('"'));
-  keyboardCombo->setCurrentIndex(keyboardCombo->findText(kb));
+  if (keyboardCombo->findText(kb) != -1) {
+    keyboardCombo->setCurrentIndex(keyboardCombo->findText(kb));
+  } else {
+    keyboardCombo->setCurrentIndex(keyboardCombo->findText("us"));
+  }
 
   // locale
   localeCombo->clear();
@@ -84,7 +88,11 @@ MInstall::MInstall(QWidget *parent) : QWidget(parent) {
   }
   QString locale;
   locale = getCmdOut("grep ^LANG /etc/default/locale").section('=',1);
-  localeCombo->setCurrentIndex(localeCombo->findText(locale));	
+  if (localeCombo->findText(locale) != -1) {
+    localeCombo->setCurrentIndex(localeCombo->findText(locale));
+  } else {
+    localeCombo->setCurrentIndex(localeCombo->findText("en_US"));
+  }
 
   proc = new QProcess(this);
   timer = new QTimer(this);
@@ -1222,6 +1230,12 @@ bool MInstall::setUserInfo() {
       "2 characters long. Please select\n"
       "a longer name before proceeding."));
     return false;
+  } else if (!userNameEdit->text().contains(QRegExp("^[a-z_][a-z0-9_-]*[$]?$"))) {
+    QMessageBox::critical(0, QString::null,
+      tr("The user name needs be lower case and it\n"
+      "cannot contain special characters or spaces\n"
+      "please choose another name before proceeding."));
+    return false;
   }
   if (strlen(userPasswordEdit->text().toAscii()) < 2) {
     QMessageBox::critical(0, QString::null,
@@ -1289,11 +1303,19 @@ bool MInstall::setComputerName() {
     QMessageBox::critical(0, QString::null,
       tr("Sorry your computer name needs to be\nat least 2 characters long. You'll have to\nselect a different name before proceeding."));
     return false;
+  } else if (computerNameEdit->text().contains(QRegExp("[^0-9a-zA-Z-.]|^[.-]|[.-]$|\\.\\."))) {
+    QMessageBox::critical(0, QString::null,
+      tr("Sorry your computer name contains invalid characters.\nYou'll have to select a different\nname before proceeding."));
+    return false;
   }
   // see if name is reasonable
   if (computerDomainEdit->text().length() < 2) {
     QMessageBox::critical(0, QString::null,
       tr("Sorry your computer domain needs to be at least\n2 characters long. You'll have to select a different\nname before proceeding."));
+    return false;
+  } else if (computerDomainEdit->text().contains(QRegExp("[^0-9a-zA-Z-.]|^[.-]|[.-]$|\\.\\."))) {
+    QMessageBox::critical(0, QString::null,
+      tr("Sorry your computer domain contains invalid characters.\nYou'll have to select a different\nname before proceeding."));
     return false;
   }
 
@@ -1749,6 +1771,7 @@ void MInstall::pageDisplayed(int next) {
     case 6:
       ((MMain *)mmn)->setHelpText(tr("<p><b>Computer Identity</b><br/>The computer name is a common unique name which will identify your computer if it is on a network. "
         "The computer domain is unlikely to be used unless your ISP or local network requires it.</p>"
+        "<p>The computer and domain names can contain only alphanumeric characters, dots, hyphens. They cannot contain blank spaces, start or end with hyphens</p>"
         "<p>The SaMBa Server needs to be activated if you want to use it to share some of your directories or printer "
         "with a local computer that is running MS-Windows or Mac OSX.</p>"));
       nextButton->setEnabled(true);
